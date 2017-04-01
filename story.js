2b3b092e9402f2e5670f92611fd37f92299a7d2c
Aurelius Calliste <callicodes@gmail.com>
2017-04-01 20:37:35 +0100

    Story Maker App
    Here are the components of the game:
    
    1. Create a variable and capture input - prompt("Ask a question.")
    
    2.  Create a separate variable for each piece of input
    
    3. Add an alert to tell the visitor that they're finished.
    
    alert("You're done!");
    
    4. Combine the input with other strings to create a message
    
    "You said: " + response
    
    5. Print the story to the browser window - document.write();

---
+++ story.js
@@ -0,0 +1,7 @@
+var adjective = prompt('Think of an adjective');
+var noun = prompt('Think of a noun');
+var verb = prompt('Think of a verb');
+
+
+var story = "There once was a " + adjective  + noun + "who loved to " + verb;
+document.write(story);
